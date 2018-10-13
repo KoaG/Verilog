d389896a0515696b4efaaa873e3810af04aeb204
Rahul Sharma <koag.classroom@gmail.com>
2018-10-13 18:47:11 +0530

    replaced fa_st with fa

--- Multipliers/Wallace_Mult_4bit/rca_4bit.v
+++ Multipliers/Wallace_Mult_4bit/rca_4bit.v
@@ -3,9 +3,9 @@ module rca_4bit
 output cout,[3:0]s);
 
 wire c0,c1,c2;
-fa_st fa1 (a[0],b[0],cin,s[0],c0);
-fa_st fa2 (a[1],b[1],c0,s[1],c1);
-fa_st fa3 (a[2],b[2],c1,s[2],c2);
-fa_st fa4 (a[3],b[3],c2,s[3],cout);
+fa fa1 (a[0],b[0],cin,s[0],c0);
+fa fa2 (a[1],b[1],c0,s[1],c1);
+fa fa3 (a[2],b[2],c1,s[2],c2);
+fa fa4 (a[3],b[3],c2,s[3],cout);
 
 endmodule 
