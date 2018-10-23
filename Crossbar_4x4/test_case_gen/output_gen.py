51fa450acef6b2bf1372e6ead1f21e5d47a2ee9b
Rahul Sharma <koag.classroom@gmail.com>
2018-10-23 12:52:44 +0530

    Add files via upload

--- Crossbar_4x4/test_case_gen/output_gen.py
+++ Crossbar_4x4/test_case_gen/output_gen.py
@@ -1,24 +1,36 @@
 slots = ['000','001','010','011']
 port = ['000','001','010','011']
+idx = [0,15,30,45]
 mem = [[format(0,'015b') for i in range(4)] for j in range(4)]
 
+portI = ['IPORT0','IPORT1','IPORT2','IPORT3']
 def print_input_slots(ip_list) :
-        print ("SLOT0 \t SLOT1 \t SLOT2 \t SLOT3")
+	matrix = [['','SLOT0','SLOT1','SLOT2','SLOT3']]
+	k = 0
         for i in range(4) :
-                print(format(int(ip_list[i][0:15],2),'04x'),
+                matrix.append([portI[k],format(int(ip_list[i][0:15],2),'04x'),
                         format(int(ip_list[i][15:30],2),'04x'),
                         format(int(ip_list[i][30:45],2),'04x'),
-                        format(int(ip_list[i][45:60],2),'04x'))
+                        format(int(ip_list[i][45:60],2),'04x')])
+	k = k + 1
+	print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
 
+portO = ['OPORT0','OPORT1','OPORT2','OPORT3']
 def print_output_slots(mem) :
-        print ("SLOT0 SLOT1 SLOT2 SLOT3")
+	matrix = [['','SLOT0','SLOT1','SLOT2','SLOT3']]
+	k = 0
         for i in mem :
-                print(format(int(i[0],2),'04x'),
+                matrix.append([portO[k],format(int(i[0],2),'04x'),
                         format(int(i[1],2),'04x'),
                         format(int(i[2],2),'04x'),
-                        format(int(i[3],2),'04x'))
+                        format(int(i[3],2),'04x')])
+		k = k + 1
+	print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
+	matrix = matrix[1:]
+	matrix = [row[1:] for row in matrix]
+	file_out.write('\n'.join([''.join([str(cell) for cell in row]) for row in matrix])+'\n')
 
-def gen_outputs(ip,port,slot,mem) :
+def gen_output(ip,port,slot,mem) :
         if(ip[0] == '1') :
                 mem = ip[0] + port + slot + ip[7:]
         return mem
@@ -34,14 +46,23 @@ def get_outputs(i0,i1,i2,i3) :
                 mem[int(ip1[i][1:4],2)][int(ip1[i][4:7],2)] = gen_output(ip1[i],'001',slots[i],mem[int(ip1[i][1:4],2)][int(ip1[i][4:7],2)])
                 mem[int(ip2[i][1:4],2)][int(ip2[i][4:7],2)] = gen_output(ip2[i],'010',slots[i],mem[int(ip2[i][1:4],2)][int(ip2[i][4:7],2)])
                 mem[int(ip3[i][1:4],2)][int(ip3[i][4:7],2)] = gen_output(ip3[i],'011',slots[i],mem[int(ip3[i][1:4],2)][int(ip3[i][4:7],2)])
-                print_output_slots(mem)
+	print_output_slots(mem)
 
-file = open("test_cases.txt",'r')
-for i in range(4) :
-        i0 = file.readline()
-        i1 = file.readline()
-        i2 = file.readline()
-        i3 = file.readline()
-        print_input_slots([i0,i1,i2,i3])
-        print_output_slots(i0,i1,i2,i3)
-file.close()
+file_cases = open("test_cases.txt",'r')
+file_out = open("output.txt",'w')
+for i in range(256) :
+	#print(file.readline())
+        i0 = format(int(file_cases.readline(),16),'064b')
+        i1 = format(int(file_cases.readline(),16),'064b')
+        i2 = format(int(file_cases.readline(),16),'064b')
+        i3 = format(int(file_cases.readline(),16),'064b')
+	#print(i0)
+	i0 = "".join([i0[1+j*16:16+j*16] for j in range(4)])
+	i1 = "".join([i1[1+j*16:16+j*16] for j in range(4)])
+	i2 = "".join([i2[1+j*16:16+j*16] for j in range(4)])
+	i3 = "".join([i3[1+j*16:16+j*16] for j in range(4)])
+ 	#print(i0)
+	#print_input_slots([i0,i1,i2,i3])
+        get_outputs(i0,i1,i2,i3)
+file_cases.close()
+file_out.close()
---
+++ Crossbar_4x4/test_case_gen/random_test_case.py
@@ -0,0 +1,78 @@
+from random import randint as rand
+l = [0x00,0x01,0x02,0x03,0x08,0x09,0x0A,0x0B,
+     0x10,0x11,0x12,0x13,0x18,0x19,0x1A,0x1B,
+     0x40,0x41,0x42,0x43,0x48,0x49,0x4A,0x4B,
+     0x50,0x51,0x52,0x53,0x58,0x59,0x5A,0x5B]
+
+rl = 0
+rh = 24
+
+pyl_i0s0 = format(0xA1,'08b')
+pyl_i0s1 = format(0xA2,'08b')
+pyl_i0s2 = format(0xA3,'08b')
+pyl_i0s3 = format(0xA4,'08b')
+pyl_i1s0 = format(0xB1,'08b')
+pyl_i1s1 = format(0xB2,'08b')
+pyl_i1s2 = format(0xB3,'08b')
+pyl_i1s3 = format(0xB4,'08b')
+pyl_i2s0 = format(0xC1,'08b')
+pyl_i2s1 = format(0xC2,'08b')
+pyl_i2s2 = format(0xC3,'08b')
+pyl_i2s3 = format(0xC4,'08b')
+pyl_i3s0 = format(0xD1,'08b')
+pyl_i3s1 = format(0xD2,'08b')
+pyl_i3s2 = format(0xD3,'08b')
+pyl_i3s3 = format(0xD4,'08b')
+
+def get_iport0(i1,i2,i3):
+	r = rand(rl,rh)
+        i0s0i = str(format(l[r],'08b')) + pyl_i0s0
+        r = rand(rl,rh) 
+        i0s1i = str(format(l[r],'08b')) + pyl_i0s1
+        r = rand(rl,rh)
+        i0s2i = str(format(l[r],'08b')) + pyl_i0s2
+        r = rand(rl,rh)
+        i0s3i = str(format(l[r],'08b')) + pyl_i0s3
+        file.write(format(int(i0s0i+i0s1i+i0s2i+i0s3i,2),'016x')+'\n')
+        file.write(i1 + '\n')
+        file.write(i2 + '\n')
+        file.write(i3 + '\n')
+
+def get_iport1(i2,i3):
+        r = rand(rl,rh)
+        i1s0i = str(format(l[r],'08b')) + pyl_i1s0
+        r = rand(rl,rh)
+        i1s1i = str(format(l[r],'08b')) + pyl_i1s1
+        r = rand(rl,rh)
+        i1s2i = str(format(l[r],'08b')) + pyl_i1s2
+        r = rand(rl,rh)
+        i1s3i = str(format(l[r],'08b')) + pyl_i1s3
+        get_iport0(format(int(i1s0i+i1s1i+i1s2i+i1s3i,2),'016x'),i2,i3)
+
+def get_iport2(i3):
+	r = rand(rl,rh)
+        i2s0i = str(format(l[r],'08b')) + pyl_i2s0
+        r = rand(rl,rh)
+        i2s1i = str(format(l[r],'08b')) + pyl_i2s1
+        r = rand(rl,rh)
+        i2s2i = str(format(l[r],'08b')) + pyl_i0s2
+        r = rand(rl,rh)
+        i2s3i = str(format(l[r],'08b')) + pyl_i2s3
+        get_iport1(format(int(i2s0i+i2s1i+i2s2i+i2s3i,2),'016x'),i3)
+
+def get_iport3():
+	r = rand(rl,rh)
+        i3s0i = str(format(l[r],'08b')) + pyl_i3s0
+        r = rand(rl,rh)
+        i3s1i = str(format(l[r],'08b')) + pyl_i3s1
+        r = rand(rl,rh)
+        i3s2i = str(format(l[r],'08b')) + pyl_i3s2
+        r = rand(rl,rh)
+        i3s3i = str(format(l[r],'08b')) + pyl_i3s3
+        get_iport2(format(int(i3s0i+i3s1i+i3s2i+i3s3i,2),'016x'))
+
+
+file = open("test_cases.txt","w")
+for i in range(256) :
+	get_iport3()
+file.close()
